f2362ab9f9606638d1295cfa902468bece06e7dd
alderfurtado <aldermarcel12@gmail.com>
2025-06-28 12:43:14 -0300

    fix: access error from OpenAiResponse

--- openaikmp/src/commonMain/kotlin/com/github/alder/furtado/openaikmp/domain/entity/OpenAiResponse.kt
+++ openaikmp/src/commonMain/kotlin/com/github/alder/furtado/openaikmp/domain/entity/OpenAiResponse.kt
@@ -1,6 +1,6 @@
 package com.github.alder.furtado.openaikmp.domain.entity
 
-sealed class OpenAiResponse(val content: String?, error: String?){
+sealed class OpenAiResponse(val content: String?,val error: String?){
     class OpenAiResponseSucceed(private val response:  String) : OpenAiResponse(content = response, error = null)
-    class OpenAiResponseError(private val error:  String) : OpenAiResponse(content = null, error = error)
+    class OpenAiResponseError(private val response: String) : OpenAiResponse(content = null, error = response)
 }
