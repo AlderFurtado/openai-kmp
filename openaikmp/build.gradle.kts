e9f3e991c7876647a6ff056e33c45666390a06dc
alderfurtado <aldermarcel12@gmail.com>
2025-07-03 09:01:39 -0300

    build: update build.gradle.kts to publish

--- openaikmp/build.gradle.kts
+++ openaikmp/build.gradle.kts
@@ -1,10 +1,13 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     kotlin("plugin.serialization") version "2.1.21"
     id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.30.0"
 }
 
 group = "io.github.alderfurtado"
@@ -66,4 +69,48 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+}
+
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.alderfurtado",
+        artifactId = "openaikmp",
+        version = "1.0.0"
+    )
+
+    // Configure POM metadata for the published artifact
+    pom {
+        name.set("OpenAi KMP")
+        description.set("OpenAi KMP is a lightweight library that streamlines integration with the OpenAI API using a simple and developer-friendly approach.")
+        inceptionYear.set("2025")
+        url.set("https://github.com/AlderFurtado/openai-kmp")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        // Specify developers information
+        developers {
+            developer {
+                id.set("alder-furtado")
+                name.set("Alder Furtado")
+                url.set("https://github.com/AlderFurtado")
+            }
+        }
+
+        // Specify SCM information
+        scm {
+            url.set("https://github.com/AlderFurtado/openai-kmp")
+            connection = "https://github.com/AlderFurtado/openai-kmp"
+            developerConnection = "https://github.com/AlderFurtado/openai-kmp"
+        }
+    }
+    // Configure publishing to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    // Enable GPG signing for all publications
+    signAllPublications()
 }
